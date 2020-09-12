4d83c2555d583bcd884f5357c571cf1bed749e7f
devWiseguy <hillj1089@gmail.com>
2020-09-12 23:49:33 +0200

    section appbar updated

--- lib/views/Home/home.dart
+++ lib/views/Home/home.dart
@@ -30,18 +30,47 @@ class _HomeState extends State<Home> {
     return Scaffold(
       backgroundColor: Colors.blueGrey[100],
       appBar: AppBar(
+        leading: IconButton(
+          onPressed: () {
+            sharedPreferences.clear();
+            sharedPreferences.commit();
+            Navigator.of(context).pushAndRemoveUntil(MaterialPageRoute(builder: (context) => Login(),), (route) => false);
+            print(sharedPreferences.getString("token"));
+          },
+          icon: Icon(Icons.exit_to_app),
+          alignment: Alignment.center,
+          color: Colors.green[400],
+          iconSize: 30.0,
+        ),
         backgroundColor: Colors.white,
         title: Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
+          // Image.network("https://cdn.discordapp.com/attachments/473218411670011904/753232212635287652/watermelon.png"),
+          //   SizedBox(width: 10.0,),
             Text(
               "SMARTWAYDIET", 
               style: TextStyle(
                 color: Colors.black
               ),
-            )
+            ),
+            // SizedBox(width: 10.0), 
+            // Image.network("https://cdn.discordapp.com/attachments/473218411670011904/753340397173997699/tomato.png")
           ],
         ),
+        actions: [
+          IconButton(
+            onPressed: () {
+              
+            },
+            icon: Icon(
+              Icons.person_outline,
+              size: 32.0,
+              color: Colors.green[400],
+            ),
+            
+          )
+        ],
         elevation: 0.0,
       ),
       body: SingleChildScrollView(
