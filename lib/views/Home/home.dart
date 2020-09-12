455d306e82aedb0a7cc86bead8ea9a78ca83e3d6
devWiseguy <hillj1089@gmail.com>
2020-09-12 22:10:33 +0200

    check token after login

--- lib/views/Home/home.dart
+++ lib/views/Home/home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:smartwaydiet/views/Auth/login.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -6,11 +8,48 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  SharedPreferences sharedPreferences;
+
+    checkLoginStatus() async {
+     sharedPreferences = await SharedPreferences.getInstance(); 
+     if(sharedPreferences.getString("token") == null) {
+       Navigator.of(context).pushAndRemoveUntil(MaterialPageRoute(builder: (BuildContext context) => Login()), (route) => false);
+     } else {
+       return Home();
+     }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    checkLoginStatus();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.blueGrey[100],
       appBar: AppBar(
-        title: Text("Home"),
+        backgroundColor: Colors.white,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "SMARTWAYDIET", 
+              style: TextStyle(
+                color: Colors.black
+              ),
+            )
+          ],
+        ),
+        elevation: 0.0,
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          child: Container(
+
+          ),
+        ),
       ),
     );
   }
